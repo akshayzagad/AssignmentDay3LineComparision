60678450f8772526f0733034c72cbeb3a1b014b1
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-30 08:05:07 +0530

    Added;Refactor The Code Using OOPS Concept

--- src/LineComparision.java
+++ src/LineComparision.java
@@ -1,43 +1,47 @@
 import java.util.Scanner;
 
 public class LineComparision {
-        public static void main(String[] args) {
-            int x1, y1, x2, y2;
-            int a1, a2, b1, b2;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2");
-            System.out.println("Enter the co-ordinates of line!!: a1,a2,b1,b2");
-            x1 = scanner.nextInt();
-            y1 = scanner.nextInt();
-            x2 = scanner.nextInt();
-            y2 = scanner.nextInt();
-            a1 = scanner.nextInt();
-            b1 = scanner.nextInt();
-            a2 = scanner.nextInt();
-            b2 = scanner.nextInt();
-            int lineOneLength = lineLenghtCal(x1, y1, x2, y2);
-            int lineSecondLength = lineLenghtCal(a1, b1, a2, b2);
-            System.out.println("The length of line 1 is: " + lineOneLength);
-            System.out.println("The length of line 2 is: " + lineSecondLength);
-            if (String.valueOf(lineOneLength).equals(String.valueOf(lineSecondLength))){
-                System.out.println("Two lines are equals");
-            }else {
-                System.out.println("Two lines are not equals");
-            }
-            compareTheLines(lineOneLength,lineSecondLength);
-        }
-
-        static int lineLenghtCal(int x1, int x2, int y1, int y2) {
-            int lineLength = (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-            return lineLength;
-        }
-    public static void compareTheLines( int lineOneLength, int lineSecondLength){
-
-        if(Double.compare(lineOneLength,lineSecondLength)==0)
-        System.out.println("equals");
-        else if (Double.compare(lineOneLength,lineSecondLength)>0)
-            System.out.println("line 1 Greater then line 2");
-        else
-            System.out.println("line 1 lesser then line 2");
-    }
+//        public static void main(String[] args) {
+//            int x1, y1, x2, y2;
+//            int a1, a2, b1, b2;
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Enter the co-ordinates of line!!: x1,y1,x2,y2");
+//            System.out.println("Enter the co-ordinates of line!!: a1,a2,b1,b2");
+//            x1 = scanner.nextInt();
+//            y1 = scanner.nextInt();
+//            x2 = scanner.nextInt();
+//            y2 = scanner.nextInt();
+//            a1 = scanner.nextInt();
+//            b1 = scanner.nextInt();
+//            a2 = scanner.nextInt();
+//            b2 = scanner.nextInt();
+//          Integer lineOneLength = lineLenghtCal(x1, y1, x2, y2);
+//            Integer lineSecondLength = lineLenghtCal(a1, b1, a2, b2);
+//            System.out.println("The length of line 1 is: " + lineOneLength);
+//            System.out.println("The length of line 2 is: " + lineSecondLength);
+//            if (lineOneLength.equals(lineSecondLength)){
+//                System.out.println("Two lines are equals");
+//            }else {
+//                System.out.println("Two lines are not equals");
+//            }
+//            compareTheLines(lineOneLength,lineSecondLength);
+//        }
+//
+//        static int lineLenghtCal(int x1, int x2, int y1, int y2) {
+//            int lineLength = (int) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+//            return lineLength;
+//        }
+//         public static void compareTheLines( Integer lineOneLength, Integer lineSecondLength){
+//
+//        if((lineOneLength.compareTo(lineSecondLength))==0){
+//             System.out.println("equals");
+//        }
+//        else if ((lineOneLength.compareTo(lineSecondLength))>0) {
+//            System.out.println("line 1 Greater then line 2");
+//        }
+//        else{
+//            System.out.println("line 1 lesser then line 2");
+//        }
+//
+//        }
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Line line1=new Line();
+        System.out.println("Get the Points for Line 1: ");
+        line1.getThePointForLine();
+        System.out.println("Get the Points for Line 2: ");
+        Line line2=new Line();
+        line2.getThePointForLine();
+        System.out.println("Length1 "+line1.calculationForLines());
+        System.out.println("Length2 "+line2.calculationForLines());
+        Line.checkEqualityOfLines(line1,line2);
+        Line.compareTheLines(line1,line2);
     }
 }
